2bfb4ae79a77a81daddb9e9dc27891071d1f1b45
Sahiba <sahibakhatoon@alphabuy.in>
2025-04-22 11:08:46 +0530

    Normalize line endings

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   images: {
     domains: ['picsum.photos'],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.module\.css$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig 
